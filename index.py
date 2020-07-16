0c97bec4321f7f19ec4fcdf72b2c988d085ff628
felipetempus <felipemouradev@gmail.com>
2020-07-16 14:02:45 -0400

    done

---
+++ index.py
@@ -0,0 +1,23 @@
+from appliances import DishWasher
+from appliances import Dryer
+from appliances import Washer
+from appliances import Refrigerator
+from appliances import CoffeeMaker
+from appliances import CanOpener
+
+whirlpool_dishwasher = DishWasher("black")
+whirlpool_dishwasher.wash_dishes()
+
+samsung_washer = Washer("red")
+samsung_dryer = Dryer("red", "gas")
+
+lg_fridge = Refrigerator("stainless")
+lg_fridge.make_ice()
+
+mr_coffee = CoffeeMaker("white")
+mr_coffee.make_coffee()
+
+my_can = CanOpener("Red")
+my_can.open_can()
+
+
